4f498ce39b32d47c9b59f74c1a53929ee3b4ac7d
eschechter <schechter.evan@gmail.com>
2019-08-15 16:23:58 -0400

    make post action for user_book and make cors all cross-domain access

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
-
 class User < ApplicationRecord
   BASE_URL = "https://www.googleapis.com/books/v1/volumes"
 
@@ -9,12 +8,16 @@ class User < ApplicationRecord
     resp = RestClient::Request.execute(url: url, method: "GET")
     resp_obj = JSON.parse(resp)
 
-    resp_obj["items"].map do |item|
-      {
-        title: item["volumeInfo"]["title"],
-        author: item["volumeInfo"]["authors"][0],
-        image: item["volumeInfo"]["imageLinks"]["thumbnail"]
-      }
+    if resp_obj["items"]
+      resp_obj["items"].map do |item|
+        {
+          title: item["volumeInfo"]["title"],
+          author: item["volumeInfo"]["authors"][0],
+          image: item["volumeInfo"]["imageLinks"]["thumbnail"]
+        }
+      end
+    else
+      []
     end
   end
 
