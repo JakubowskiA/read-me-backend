cd24501d0155d25bd2cf6acfd22c194f99307562
eschechter <schechter.evan@gmail.com>
2019-08-16 12:00:32 -0400

    add route fo displaying a detailed book view with description

--- app/models/user_book.rb
+++ app/models/user_book.rb
@@ -1,7 +1,7 @@
 class UserBook < ApplicationRecord
   BASE_URL = "https://www.googleapis.com/books/v1/volumes"
   DEFAULT_IMAGE = "http://spiritualmilk.com/wp-content/uploads/2017/03/genericBookCover.jpg"
-
+  include ActionView::Helpers::SanitizeHelper
   belongs_to :user
   # also belongs to book but Google Books ...
 
@@ -13,7 +13,22 @@ class UserBook < ApplicationRecord
     {
       title: resp_obj["volumeInfo"]["title"],
       author: resp_obj["volumeInfo"]["authors"][0],
-      image: item["volumeInfo"]["imageLinks"] ? item["volumeInfo"]["imageLinks"]["thumbnail"] : DEFAULT_IMAGE
+      image: resp_obj["volumeInfo"]["imageLinks"] ? resp_obj["volumeInfo"]["imageLinks"]["thumbnail"] : DEFAULT_IMAGE
+    }
+  end
+
+  def fetch_detailed_book_info
+    url = "#{BASE_URL}/#{book_id}"
+    resp = RestClient::Request.execute(url: url, method: "GET")
+    resp_obj = JSON.parse(resp)
+
+    {
+      title: resp_obj["volumeInfo"]["title"],
+      author: resp_obj["volumeInfo"]["authors"][0],
+      image: resp_obj["volumeInfo"]["imageLinks"] ? resp_obj["volumeInfo"]["imageLinks"]["medium"] : DEFAULT_IMAGE,
+      description: resp_obj["volumeInfo"]["description"] ?
+        strip_tags(resp_obj["volumeInfo"]["description"]) :
+        "No description available"
     }
   end
 end
