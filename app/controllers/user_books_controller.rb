0d6e7c980f0e1e10e47224981d7522ea8c84f8c0
eschechter <schechter.evan@gmail.com>
2019-08-19 10:38:55 -0400

    make it so backend returns empty objects when given request for non-existent user-book

--- app/controllers/user_books_controller.rb
+++ app/controllers/user_books_controller.rb
@@ -1,13 +1,20 @@
 class UserBooksController < ApplicationController
   def show
     user_book = UserBook.find_by(user_id: params[:user_id], book_id: params[:book_id])
-    render json: user_book.fetch_book_info
+    if user_book.nil?
+      render json: {}
+    else
+      render json: user_book.fetch_book_info
+    end
   end
 
   def show_detail
-    byebug
     user_book = UserBook.find_by(user_id: params[:user_id], book_id: params[:book_id])
-    render json: user_book.fetch_detailed_book_info
+    if user_book.nil?
+      render json: {}
+    else
+      render json: user_book.fetch_detailed_book_info
+    end
   end
 
   def create
