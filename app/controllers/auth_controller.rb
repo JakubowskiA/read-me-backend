11c7457e9d45c7769fd84a27307057a1a8634a5f
eschechter <schechter.evan@gmail.com>
2019-08-20 14:42:01 -0400

    implement route and action for users to login

--- app/controllers/auth_controller.rb
+++ app/controllers/auth_controller.rb
@@ -8,6 +8,16 @@ class AuthController < ApplicationController
     render json: { name: user.name, email: user.email, id: user_id }
   end
 
+  def create
+    user = User.find_by(email: params[:email])
+    if user && user.authenticate(params[:password])
+      token = JWT.encode({ user_id: user.id }, "secret")
+      render json: { token: token, user: { id: user.id, email: user.email, name: user.name } }
+    else
+      render json: {}
+    end
+  end
+
   def destroy
   end
 end
